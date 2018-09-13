1f0fdbf0e68b9a68baf4f24a3984db5542079d2a
Junichi TANAKA <junsan45@gmail.com>
2018-09-13 19:15:16 +0900

    Simplify promises
    * reformat

--- node-8.js
+++ node-8.js
@@ -1,41 +1,37 @@
-
 const request = require('request-promise-native');
 
 function http_get(url) {
-  return request(url)
-    .then(function (body) {
-      console.log("length:" + body.length);
-    })
-    .catch(function (err) {
-      console.error(err.toString());
-    });
+    return request(url)
+        .then(function (body) {
+            console.log("length:" + body.length);
+        })
+        .catch(function (err) {
+            console.error(err.toString());
+        });
 }
 
 function main() {
-  console.log("start");
-  const p = new Promise((resolve, reject) => {
-    resolve();
-  })
-  .then(() => {
-    console.log("1");
-  })
-  .then(async () => {
-    console.log("2");
-    await http_get('https://www.google.com');
-  })
-  .then(() => {
-    console.log("3");
-  })
-  .catch(err => {
-    console.error("catch:" + err);
-  })
-  .then(() => {
-    console.log("callback");
-  });
-  return p;
+    console.log("start");
+    return Promise.resolve()
+        .then(() => {
+            console.log("1");
+        })
+        .then(async () => {
+            console.log("2");
+            await http_get('https://www.google.com');
+        })
+        .then(() => {
+            console.log("3");
+        })
+        .catch(err => {
+            console.error("catch:" + err);
+        })
+        .then(() => {
+            console.log("callback");
+        });
 }
 
-(async function() {
-  await main();
-  console.log('fin')
+(async function () {
+    await main();
+    console.log('fin')
 })();
