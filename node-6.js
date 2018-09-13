abfc420b94ed334cc042ac9712e682f4cbf9bddd
Junichi TANAKA <junsan45@gmail.com>
2018-09-13 19:25:39 +0900

    Simplify promises
    * reformat

--- node-6.js
+++ node-6.js
@@ -1,34 +1,31 @@
-
 const request = require('request-promise-native');
 
 function http_get(url) {
-  return request(url).promise()
-    .then(function (body) {
-      console.log("length:" + body.length);
-    })
-    .catch(function (err) {
-      console.error(err.toString());
-    });
+    return request(url).promise()
+        .then(function (body) {
+            console.log("length:" + body.length);
+        })
+        .catch(function (err) {
+            console.error(err.toString());
+        });
 }
 
 console.log("start");
-const p = new Promise((resolve, reject) => {
-  resolve();
-})
-.then(() => {
-  console.log("1");
-})
-.then(() => {
-  console.log("2");
-  return http_get('https://www.google.com');
-})
-.then(() => {
-  console.log("3");
-})
-.catch(err => {
-  console.error("catch:" + err);
-})
-.then(() => {
-  console.log("callback");
-  console.log("fin");
-});
+Promise.resolve()
+    .then(() => {
+        console.log("1");
+    })
+    .then(() => {
+        console.log("2");
+        return http_get('https://www.google.com');
+    })
+    .then(() => {
+        console.log("3");
+    })
+    .catch(err => {
+        console.error("catch:" + err);
+    })
+    .then(() => {
+        console.log("callback");
+        console.log("fin");
+    });
